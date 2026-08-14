15beadc8d14fbf509097377fe83a176e3d00bcd0
aikev <aike@gmail.com>
2026-08-14 13:47:19 +0800

    chore: 清理临时脚本

--- .arb-cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /home/aikev/code/arboris-novel
-rm -f .arb-commit.sh .arb-check.sh .arb-verify.sh .arb-deploy.sh
-git add -A
-git commit -q -m "chore: 移除部署验证用的临时脚本"
-git push -q origin main
-git status --short
-git log --oneline -1
